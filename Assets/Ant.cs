3cb1469cf320f971cd8ae59799d203eacb4b5ff1
Matthieu <matthieunadeau96@gmail.com>
2022-09-08 14:32:43 -0400

    Fix Ant Pathfinding and Food pickup. Add gitignore

--- Assets/Ant.cs
+++ Assets/Ant.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Ant : MonoBehaviour
 {
@@ -19,6 +20,7 @@ public class Ant : MonoBehaviour
 
     public LayerMask foodMask;
     public LayerMask obstructionMask;
+    public LayerMask takenFoodLayer;
 
     public bool canSeeFood;
 
@@ -30,12 +32,13 @@ public class Ant : MonoBehaviour
 
     private void Start()
     {
+        //targetFood = GameObject.FindGameObjectWithTag("Food");
         StartCoroutine(FOVRoutine());
     }
 
     void Update()
     {
-        targetFood = GameObject.FindGameObjectWithTag("Food");
+        Debug.Log("Can See Food ? " + canSeeFood );
         HandleMovement();
         
 
@@ -44,11 +47,10 @@ public class Ant : MonoBehaviour
 
     private void HandleMovement()
     {
-        //desiredDirection = ((Vector3)target.position - position).normalized;
         if (!canSeeFood)
-        {
+        { 
+            // Wander around
             dd = (dd + Random.insideUnitCircle * wanderStrength).normalized;
-
             desiredDirection = new Vector3(dd.x, 0, dd.y);
         }
            
@@ -79,75 +81,53 @@ public class Ant : MonoBehaviour
     void HandleFood()
     {
          //Get all food objects within perception radius
-        Collider[] rangeChecks = Physics.OverlapSphere(position, viewRadius, foodMask);
-
-        if (rangeChecks.Length != 0)
+        if(targetFood == null)
         {
-            //Transform target = rangeChecks[Random.Range(0, rangeChecks.Length)].transform;
+            Collider[] rangeChecks = Physics.OverlapSphere(position, viewRadius, foodMask);
 
-            Transform target = rangeChecks[0].transform;
-            Vector3 directionToTarget = (target.position - transform.position).normalized;
-
-            if (Vector3.Angle(transform.forward, directionToTarget) < viewAngle / 2)
+            if (rangeChecks.Length > 0)
             {
-                float distanceToTarget = Vector3.Distance(transform.position, target.position);
-                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                Debug.Log("Range Check Length = " + rangeChecks.Length);
+                //Transform target = rangeChecks[Random.Range(0, rangeChecks.Length)].transform;
+
+                Transform target = rangeChecks[Random.Range(0, rangeChecks.Length)].transform;
+                Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+                if (Vector3.Angle(transform.forward, directionToTarget) < viewAngle / 2)
                 {
-                    desiredDirection = (target.position - transform.position).normalized;
-                    Debug.Log("!!!!!!! Heading to -> " + desiredDirection);
-                    canSeeFood = true;
-                    Debug.Log("$$$ distance to target = " + distanceToTarget);
-                    // try picking up the food it is close enough
-                    if (distanceToTarget < foodPickupRadius)
-                    {
-                        Debug.Log(">> PICKUP!");
-                        targetFood.transform.position = head.position;
-                        targetFood.transform.parent = head;
-                        targetFood = null;
-                    }
+                    //target.gameObject.layer = takenFoodLayer;
+                    targetFood = target.gameObject;
+                    Debug.Log(" I can see gameobject -> " + targetFood.name);
                 }
                 else
                     canSeeFood = false;
             }
+            else if (canSeeFood)
+                canSeeFood = false;
+        } 
+        else
+        {
+            Vector3 directionToTarget = (targetFood.transform.position - transform.position).normalized;
+            float distanceToTarget = Vector3.Distance(transform.position, targetFood.transform.position);
+
+            if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+            {
+                desiredDirection = (targetFood.transform.position - transform.position).normalized;
+                Debug.Log("!!!!!!! Heading to -> " + desiredDirection);
+                canSeeFood = true;
+                Debug.Log("$$$ distance to target = " + distanceToTarget);
+                // try picking up the food it is close enough
+                if (distanceToTarget < foodPickupRadius)
+                {
+                    Debug.Log(">> PICKUP!");
+                    canSeeFood = false;
+                    targetFood.transform.position = head.position;
+                    targetFood.transform.parent = head;
+                    targetFood = null;
+                }
+            }
             else
                 canSeeFood = false;
         }
-        else if (canSeeFood)
-            canSeeFood = false;
-
-        //if(targetFood == null)
-        //{
-        //    // Getall food objects within perception radius
-        //    Collider[] allFood = Physics.OverlapSphere(position, viewRadius, foodLayer);
-        //    Debug.Log("all food -> " + allFood);
-
-        //    if (allFood.Length > 0)
-        //    {
-        //        // Select one of the food objects at random
-        //        Transform food = allFood[Random.Range(0, allFood.Length)].transform;
-        //        Vector3 dirToFood = (food.position - head.position).normalized; // this might cause issues with y axis
-                
-        //        // Start targeting the food if it is within view angle
-        //        if (Vector3.Angle(forward, dirToFood) < viewAngle / 2)
-        //        {
-        //            //food.gameObject.layer = Layers.takenFoodLayer;
-        //            targetFood = food;
-        //        }
-        //    }
-        //} else
-        //{
-        //    Debug.Log("FOUND!");
-        //    // Try move towards the target food
-        //    desiredDirection = (targetFood.position - head.position).normalized;
-
-        //    // Pick up the food if it is close enough
-        //    const float foodPickupRadius = 0.05f;
-        //    if(Vector3.Distance(targetFood.position, head.position) < foodPickupRadius)
-        //    {
-        //        targetFood.position = head.position;
-        //        targetFood.parent = head;
-        //        targetFood = null;
-        //    }
-        //}
     }
 }
